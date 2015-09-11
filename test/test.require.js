b63715e166c7037695da1ead57c619b7636fa210
andi-neck <andi.neck@intesso.com>
2015-09-11 07:44:55 +0100

    adds require strategies test

---
+++ test/test.require.js
@@ -0,0 +1,33 @@
+var test = require('tape');
+var Locale = require('../server');
+
+test('require all strategies with Arrays', function(t){
+  try{
+    var locale = Locale({
+      locales: ['de', 'de-ch', 'en', 'en-GB', 'en-us'],
+      getLocaleFrom: ['subdomain', 'query', 'path', 'cookie', 'acceptLanguage', 'default'],
+      storeLocaleTo: ['cookie', 'session', 'request', 'locals']
+    });
+    t.true(locale);
+    t.end();
+  } catch(e) {
+    t.fail('initialization failed', e.message);
+    t.end(e);
+  }
+});
+
+
+test('require all strategies with Strings', function(t){
+  try{
+    var locale = Locale({
+      locales: 'de,de-ch,en,en-GB,en-us',
+      getLocaleFrom: 'subdomain,query,path,cookie,acceptLanguage,default',
+      storeLocaleTo: 'cookie,session,request,locals'
+    });
+    t.true(locale);
+    t.end();
+  } catch(e) {
+    t.fail('initialization failed', e.message);
+    t.end(e);
+  }
+});
